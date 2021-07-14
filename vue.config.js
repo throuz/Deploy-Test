4a73fa30a92d57ddf39d917faef7aeaf101de9fe
throuz <64568129+throuz@users.noreply.github.com>
2021-07-14 20:28:34 +0800

    test deploy forge

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/Deploy-Test/" : "/",
+    // publicPath: process.env.NODE_ENV === "production" ? "/Deploy-Test/" : "/",
+    publicPath: "/",
 };
