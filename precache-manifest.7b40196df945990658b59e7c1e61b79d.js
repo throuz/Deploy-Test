943caed51a0246c0d73e8f25a9a1e8a6d65a1b96
throuz <64568129+throuz@users.noreply.github.com>
2021-07-15 07:47:26 +0800

    deploy

---
+++ precache-manifest.7b40196df945990658b59e7c1e61b79d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c076b6b30161a7982c3",
+    "url": "/css/app.792d0e2c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dde04d7e49fc4fa9c696b694bd852445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0950501d66e85c2e060",
+    "url": "/js/about.66489cda.js"
+  },
+  {
+    "revision": "6c076b6b30161a7982c3",
+    "url": "/js/app.c0c59a56.js"
+  },
+  {
+    "revision": "6cfd52cf9e18a00479a2",
+    "url": "/js/chunk-vendors.f7c6fe0b.js"
+  },
+  {
+    "revision": "30f6b9f76be8bf3ece1299a2f0edf42e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
